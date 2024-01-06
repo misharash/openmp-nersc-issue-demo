0990c1b0f04f62e65724bedffb707579c42bc979
Michael Rashkovetskyi <misharash@gmail.com>
2024-01-06 17:46:45 -0500

    Straightforward batch script

---
+++ demo0.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=desi
+#SBATCH --constraint=cpu
+#SBATCH --qos=regular
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --job-name=openmp-issue-demo0
+
+# OpenMP settings
+export OMP_PROC_BIND=spread
+export OMP_PLACES=threads
+export OMP_NUM_THREADS=256
+
+./demo $(OMP_NUM_THREADS) $(python3 -c 'print(10**10)')
