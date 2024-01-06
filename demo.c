01823b608bdb79f8b38542fac400e74fd373d171
Michael Rashkovetskyi <misharash@gmail.com>
2024-01-06 12:28:28 -0500

    Added timing

--- demo.c
+++ demo.c
@@ -3,14 +3,19 @@
 #include <stdio.h>
 #include <omp.h>
 #include <sched.h>
+#include <time.h>
 
 int main(int argc, char* argv[]) {
     assert(argc == 3);
     int n_threads = atoi(argv[1]);
     omp_set_num_threads(n_threads);
     int n_per_thread = atoi(argv[2]);
+    clock_t clock_time;
     #pragma omp parallel
-    if (omp_get_thread_num() == 0) printf("Running with %d threads, %d iterations on each\n", omp_get_num_threads(), n_per_thread);
+    if (omp_get_thread_num() == 0) {
+        printf("Running with %d threads, %d iterations on each\n", omp_get_num_threads(), n_per_thread);
+        clock_time = clock();
+    }
     #pragma omp for
     for (int t = 0; t < n_threads; t++) {
         int s = 0, x = 0, a = 0, o = 0;
@@ -21,5 +26,8 @@ int main(int argc, char* argv[]) {
             a &= i;
             o |= i;
         }
+    }
+    if (omp_get_thread_num() == 0) {
+        printf("Elapsed time: %lf s\n", ((double)(clock() - clock_time)) / CLOCKS_PER_SEC);
     }
 }
