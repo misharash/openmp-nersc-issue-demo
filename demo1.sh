096b1cac0580fa8cdccec4a678b63bb3d349f38d
Michael Rashkovetskyi <misharash@gmail.com>
2024-01-06 22:10:47 -0500

    Updated time limit

--- demo1.sh
+++ demo1.sh
@@ -2,7 +2,7 @@
 #SBATCH --account=desi
 #SBATCH --constraint=cpu
 #SBATCH --qos=regular
-#SBATCH --time=00:30:00
+#SBATCH --time=00:05:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --job-name=openmp-issue-demo1
