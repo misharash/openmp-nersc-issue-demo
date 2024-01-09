857092ba89a673f066c1691a2f5f75ae4da973fe
Michael Rashkovetskyi <misharash@gmail.com>
2024-01-08 20:00:25 -0500

    Print environment before and after

--- demo.py
+++ demo.py
@@ -1,4 +1,7 @@
 import os
+
+print(os.environ)
+
 import numpy as np
 from sklearn.cluster import KMeans
 
@@ -6,6 +9,8 @@ np.random.seed(42)
 X, Y = np.random.rand(2, 10**5, 2)
 kmeans = KMeans(n_clusters = 60, random_state = 42).fit(X)
 Y_labels = kmeans.predict(Y)
+
+print(os.environ)
 
 os.environ["OMP_PLACES"] = ",".join("{" + str(i) + "},{" + str(i+128) + "}" for i in range(128))
 os.system(f"./demo 256 {10**8}")
