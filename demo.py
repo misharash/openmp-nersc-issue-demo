c21360500aec49b9da683991189eb7408219d450
Michael Rashkovetskyi <misharash@gmail.com>
2024-01-08 20:12:40 -0500

    Removed the labeling

--- demo.py
+++ demo.py
@@ -3,8 +3,7 @@ import numpy as np
 from sklearn.cluster import KMeans
 
 np.random.seed(42)
-X, Y = np.random.rand(2, 10**5, 2)
+X = np.random.rand(10**5, 2)
 kmeans = KMeans(n_clusters = 60, random_state = 42).fit(X)
-Y_labels = kmeans.predict(Y)
 
 os.system(f"./demo 256 {10**8}")
